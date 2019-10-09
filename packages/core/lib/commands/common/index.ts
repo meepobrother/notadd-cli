b88e96a26b650161cae374515980e058579faf50
imeepos <1037483576@qq.com>
2019-10-09 16:53:03 +0800

    clear workspace

--- packages/core/lib/commands/common/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 命令
- */
-export abstract class Command {
-    abstract run(): void;
-    abstract watch(): void;
-}
--- packages/core/lib/commands/compiler/index.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "../common";
-/**
- * 编译ts到js,并复制静态资源到制定文件夹
- */
-export class Compiler extends Command {
-    run() { }
-    watch() { }
-}
