c5b8f836f81cd1c6da48df1e42857c5f8d00996e
imeepos <1037483576@qq.com>
2019-10-10 13:47:18 +0800

    updage bin.ts

--- packages/core/lib/bin.ts
+++ packages/core/lib/bin.ts
@@ -1,2 +1,16 @@
 #!/usr/bin/env node
-import { } from 'yargs';
+import { command, Argv } from 'yargs';
+import { CONFIG_FILE } from './static/index';
+import { join } from 'path';
+import { existsSync } from 'fs';
+import { Config } from './config';
+const root = process.cwd();
+const configFile = join(root, CONFIG_FILE);
+const defaultConfigFile = join(__dirname, 'assets/config.json')
+let config: Config = Config.fromJson(require(defaultConfigFile))
+if (existsSync) {
+    config = Config.fromJson(require(configFile))
+}
+command('init [name]', `初始化`, (yargs: Argv) => {
+    console.log(config)
+});
