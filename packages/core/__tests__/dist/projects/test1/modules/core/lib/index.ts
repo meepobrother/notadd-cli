c982644bff78a97bd84da3b8d522fb517319529e
imeepos <1037483576@qq.com>
2019-10-10 12:57:27 +0800

    upgrade __tests__

---
+++ packages/core/__tests__/dist/projects/test1/modules/core/lib/index.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AddDesc } from './add_desc';
+import { UtilModule } from '@notadd/test1.util';
+
+@Module({
+    imports: [
+        UtilModule
+    ],
+    providers: [
+        AddDesc
+    ]
+})
+export class CoreModule { }
+
+export {
+    AddDesc
+}
---
+++ packages/core/__tests__/dist/projects/test1/modules/util/lib/util.ts
@@ -0,0 +1,8 @@
+export class Util {
+    async add(a: number, b: number) {
+        return a + b;
+    }
+    async desc(a: number, b: number) {
+        return a - b;
+    }
+}
